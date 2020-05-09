9c5920236ef2260047ddadf375e2651a07d46233
Jorge Durango <djorge.ivan97@gmail.com>
2020-05-08 22:57:08 -0500

    changed

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const db = require("mongoose");
 
 async function connect(uri) {
-  console.log(uri);
   try {
-    await db.connect(uri, {
+    const resultado = await db.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
     });
+    console.log("connect: ", resultado);
   } catch (err) {
     console.error("[db]", err.message);
   }
