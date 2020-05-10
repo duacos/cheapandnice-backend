c0add7d64e41991130bc267b6846651773f45eed
Jorge Durango <djorge.ivan97@gmail.com>
2020-05-09 22:45:35 -0500

    sameSite=Lax

--- routes/controller.js
+++ routes/controller.js
@@ -2,7 +2,7 @@ const { userModel, productModel } = require("./model");
 
 module.exports.handleUsers = (req, res, data, filter, status) => {
   res.cookie("session", data.accessToken, {
-    sameSite: true,
+    sameSite: "Lax",
     secure: false,
     httpOnly: true,
   });
